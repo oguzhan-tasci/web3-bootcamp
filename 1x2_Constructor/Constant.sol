bd4b63f853a18223c17d1c3fb59995e633c71166
oyeProjects <oyeprojects@hotmail.com>
2023-02-01 21:18:02 +0300

    constructor have learned

--- 1x2_Constructor/Constant.sol
+++ 1x2_Constructor/Constant.sol
@@ -3,7 +3,13 @@ pragma solidity ^0.8.0;
 
 contract Constants {
     
+    // constant'ı belirtirsek daha değişmeyeceği anlamını vermiş oluruz.
     address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
     uint public constant MY_UINT = 123;
+
+    // Bu fonksiyon hata verir ve çalışmaz çünkü constant değişkeni değiştirilemez.
+    // function set(uint num) public {
+    //     MY_ADDRESS = num;
+    // }
 
 }
--- 1x2_Constructor/Immutable.sol
+++ 1x2_Constructor/Immutable.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 contract Immutable {
     
+    // Immutable'da constant gibi değişmez değerlerdir.
+    // Aralarındaki en büyük fark , contant'ı en basta belirmen gerekiyor fakat immutable değerleri constructor içinde atama yapabilirsin.
     address public immutable MY_ADDRESS;
     uint public immutable MY_LUCKYNUMBER;
 
