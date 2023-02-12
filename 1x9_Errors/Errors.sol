cbef44daa9b8261441f54d2938e3202bad47e433
oyeProjects <oyeprojects@hotmail.com>
2023-02-12 14:34:42 +0300

    errors

--- 1x9_Errors/Errors.sol
+++ 1x9_Errors/Errors.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.0;
 
 contract Errors {
-    uint256 public totalBalance;
+    uint256 public totalBalance; 
     mapping(address => uint256) public userBalances;
 
+    // hata tanımlıyoruz : 
     error ExceedingAmount(address user, uint256 exceedingAmount);
     error Deny(string reason);
 
+    // receive ve fallback ile bu kontrata para gelmesini engellemek istiyoruz.
     receive() external payable {
         revert Deny("No direct payments.");
     }
@@ -24,11 +26,14 @@ contract Errors {
     }
 
     function withdraw(uint256 _amount) noZero(_amount) external {
+        // require kullanıcı sebebiyle oluşacak hataları önlemek için (ör: elinde olmayan parayı çekmeye çalışması),
+        // kontratın içindeki bir hata içinse 'assert' çalışıyor.      
         uint256 initalBalance = totalBalance;
 
         //require(userBalances[msg.sender] >= _amount, "Insufficient balance.");
 
         if(userBalances[msg.sender] < _amount) {
+            // revert ile engelliyoruz.
             //revert("Insufficient balance.");
             revert ExceedingAmount(msg.sender, _amount - userBalances[msg.sender]);
         }
@@ -38,11 +43,15 @@ contract Errors {
         // address => address payable
         payable(msg.sender).transfer(_amount);
 
+        // assert'u mantıksal hatalar için kullanıyoruz.
+        // Kontratın içerisinde hiç çalışmayacağından emin olduğumuz yerlere, güvenlik önlemi olması adına ekliyoruz.
+        // Assert'ler genellikle uygulamanın deneme sürümlerinde kullanılıyor, sonra pek gerek kalmıyor..
         assert(totalBalance < initalBalance);
     }
 
     modifier noZero(uint256 _amount) {
         require(_amount != 0);
+        // require(_amount != 0 , "hata msajı");
         _;
     }
 
