a8c903daeb14787b7361ce9b763161280a670319
oyeProjects <oyeprojects@hotmail.com>
2023-02-12 19:24:31 +0300

    inheritances

--- 1x12_Inheritance/Import.sol
+++ 1x12_Inheritance/Import.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Bu şekilde dışarıdan da miras alabiliyoruz.
+
+
 // import "@openzeppelin/contracts/access/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol"; 
 
 contract Wallet is Ownable {
     fallback() payable external {}
--- 1x12_Inheritance/Inheritance.sol
+++ 1x12_Inheritance/Inheritance.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.0;
 
 
 // virtual
-
+// virtual : Eğer bir fonksiyon 'virtual' olarak işaretleniyorsa şu anlama geliyor : 
+// Bu fonksiyon inherit alınırsa bu fonksiyonun içeriği değiştirilebilir demektir.
 contract A {
     uint public x;
 
@@ -18,7 +19,8 @@ contract A {
     }
 }
 // override
-
+// 'virtual' olan fonksiyonların içeriğini değiştirmek istiyorsak, değiştirdiğimiz fonksiyonu 'override' olarak işaretlemeliyiz.
+// A kontratindaki fonksiyonları kullanmak için A kontratını '... is A' ile inherit ediyoruz.
 contract B is A {
 
     uint public z;
@@ -31,4 +33,25 @@ contract B is A {
         x = _x + 2;
     }
 
+}
+
+// -------------------------- 
+
+contract Human {
+    function sayHello() public pure virtual returns(string memory) {
+        return "Aramiza katilmak ve kendini gelistirmek ister misin?";
+    }
+}
+
+contract SuperHuman is Human {
+    function sayHello() public pure override returns(string memory) {
+        return "Aramiza katildigin icin pisman olmayacaksin :)";
+    }
+
+    // isMember 'true' ise kendi sayHello'sunu , false ise inheritance oldugu fonksiyonun sayHello'sunu çağırır.
+    // İnheritance olan fonksiyona ulaşmak için 'super' anahtar kelimesi veya contract ismi kullanılır.
+    function welcomeMessage(bool isMember) public pure returns(string memory) {
+        return isMember ? sayHello() : super.sayHello();
+        // return isMember ? sayHello() : Human.sayHello();
+    }
 }
