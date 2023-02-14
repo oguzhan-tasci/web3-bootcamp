ae1a97066b1f72beb41bcb9cd9473e5025756166
oyeProjects <oyeprojects@hotmail.com>
2023-02-14 06:43:21 +0300

    creat contract

--- 1x16_CreatingContracts/Create.sol
+++ 1x16_CreatingContracts/Create.sol
@@ -4,11 +4,15 @@ pragma solidity ^0.8.0;
 contract VaultFactory {
     mapping(address => Vault[]) public userVaults;
 
+    // ücret almadan cüzdan yaratan fonksiyon.
     function createVault() external {
+        // constructor parametresi olmasaydı :
+        // "Vault vault = new Vault();" yeterli olurdu fakat parantez içine constructor dönmemiz gerekiyor.
         Vault vault = new Vault(msg.sender);
         userVaults[msg.sender].push(vault);
     }
 
+    // ücret alarak cüzdan yaratan fonksiyon.
     function createVaultWithPayment() external payable {
         Vault vault = (new Vault){value: msg.value}(msg.sender);
         userVaults[msg.sender].push(vault);
@@ -16,7 +20,7 @@ contract VaultFactory {
 }
 
 contract Vault {
-    address public owner;
+    address public owner; 
     uint256 private balance;
 
     constructor(address _owner) payable {
@@ -24,6 +28,12 @@ contract Vault {
         balance += msg.value;
     }
 
+    // fallback ve receive, deposit fonksiyonunu kullanmadan kontrata bir değer içeren mesaj gelirse ne yapmamız gerektiğini gösteriyor:
+
+    // eğer revert koysaydık otomatikman reddilecekti. Kontrat sadece deposit ile para yatırmaya açık olacaktı.
+    //  fallback() external payable {
+    //      revert();
+    //  } 
     fallback() external payable {
         balance += msg.value;
     }
@@ -33,12 +43,13 @@ contract Vault {
     }
 
     function getBalance() external view returns (uint256) {
+        // return address(this).balance; --> bu şekilde de olur.
         return balance;
     }
 
     function deposit() external payable {
         balance += msg.value;
-    }
+    }   
 
     function withdraw(uint256 _amount) external {
         require(msg.sender == owner, "You are not authorized.");
